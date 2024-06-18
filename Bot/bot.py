67f12a8c5056c880a81828681201a3c2baa23619
onatkarabulut <{onatk12.2@gmail.com}>
2024-06-18 17:43:04 +0300

    text change

--- Bot/bot.py
+++ Bot/bot.py
@@ -36,7 +36,20 @@ class Bot:
         return medias
 
     def post_media(self, media):
-        caption = "Bu bir testtir"
+        caption = """
+No problem! Here's the information about the Mercedes CLR GTR:
+
+The Mercedes CLR GTR is a remarkable racing car celebrated for its outstanding performance and sleek design. Powered by a potent 6.0-liter V12 engine, it delivers over 600 horsepower.
+
+Acceleration from 0 to 100 km/h takes approximately 3.7 seconds, with a remarkable top speed surprising 320 km/h.🥇
+
+Incorporating adventure aerodynamic features and cutting-edge stability technologies, the CLR GTR ensures exceptional stability and control, particularly during high-speed maneuvers. 💨
+
+Originally priced at around $1.5 million, the Mercedes CLR GTR is considered one of the most exclusive and prestigious racing cars ever produced. 💰
+
+Its limited production run of just five units adds to its rarity, making it highly sought after by racing enthusiasts and collectors worldwide. 🌎
+        """
+        
         if media.media_type == 1:  # Photo
             self.cl.photo_upload(media.path, caption)
         elif media.media_type == 2:  # Video
